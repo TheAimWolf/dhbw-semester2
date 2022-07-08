23a9c66d6bada1c9750d72978aca05cd4f97f9a8
Julian Wolf <dev@juwolf.com>
2022-07-08 21:09:25 +0200

    Selection Sort hinzugefügt
    Signed-off-by: Julian Wolf <dev@juwolf.com>

--- TheoretischeInformatik2/exercises/selection-sort.js
+++ TheoretischeInformatik2/exercises/selection-sort.js
@@ -1,10 +1,3 @@
-# Elementare Sortierverfahren
-## Selection Sort
-Selektieren und tauschen  <br>
-![Sort Gif](https://th.bing.com/th/id/R.1f66c277a7a820e3492149c6e499bdb1?rik=%2bmoXmDuAcYgtQQ&riu=http%3a%2f%2fpiratelearner.com%2fstatic%2fmedia%2fimages%2fadmin%2f2015%2f10%2f13%2fselection.gif&ehk=Htfp1HLTiCrFGkYf4E3f24eK8NBBLRqcan9EWuhND30%3d&risl=&pid=ImgRaw&r=0)
-
-### JS Code Beispiel
-```js
 function selectionSort(pList) {
     var min;
     if(isNaN(pList[0])){
@@ -26,8 +19,8 @@ function swapItemsOfList(pList, indexA, indexB) {
     pList[indexA] = pList[indexB];
     pList[indexB] = temp;
 }
-```
 
-## Insertion Sort
-
-![Insertion Gif](https://piratelearner.com/static/media/images/admin/2015/10/14/insertion.gif)
+//Test aufrufen
+var TestList = [3, 7, 2, 9, 2, 0, 1, 10];
+selectionSort(TestList);
+console.log(TestList);
--- index.md
+++ index.md
@@ -1,5 +1,11 @@
 # DHBW TIF21B Semster 2
+In diesem Reposetory sind die Aufschriebe und Projekte des 2. Semestern der Technischen Informatik für den Kurs TIF 21 hinterlegt. Ich gebe keine Garantie für die Richtigkeit der Aufschriebe. Eigentlich ist das Repo nur für mich gedacht, weshalb ich nicht besonders auf Rechtschreibung und Satzstellung geachtet habe.
 
-
-## Inhaltsverzeichnis
+## Fächer
 - [Theoretische Informatik](TheoretischeInformatik2/)
+
+## Allgemeines
+Mein persönliches Portfolio erreicht ihr unter [juwolf.com](https://juwolf.com)
+
+## Lizenz
+Macht mit dem Inhalt was ihr wollt. Wenn ihr damit Geld verdienen wollt, dann versucht es gerne :D.
